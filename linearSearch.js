12bd8753ab3c56cb73dbce0b2165e2bbc925de48
Madiyar <madeyra2980@gmail.com>
2024-08-04 13:52:32 +0500

    first commit

---
+++ linearSearch.js
@@ -0,0 +1,15 @@
+// 
+
+const arrays = [1,2,3,4,5,6,7,8,9]
+
+
+function linearSearch(arrays, item){
+
+    for (let i = 0; i < arrays.length; i++) {
+        if(arrays[i] === item){
+            return i
+        }
+    }
+}
+
+console.log(linearSearch(arrays, 4))
